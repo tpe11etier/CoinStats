0f8079e6b0f09a28204a17fb2517d594f0c2f415
Tony Pelletier <tony.pelletier@nuance.com>
2021-04-22 16:48:16 -0400

    Fix Pickler

--- src/main/scala/com/trp/coinstats/Tester.scala
+++ src/main/scala/com/trp/coinstats/Tester.scala
@@ -6,7 +6,7 @@ object Tester {
   def main(args: Array[String]): Unit = {
     println("Hello world!")
     val client = new CoinStatsClientImpl(new CoinStatsAPI)
-    //    println(client.getCoinsGlobalAvgPrices)
+        println(client.getCoinsGlobalAvgPrices)
     //    val coins = client.getCoinsGlobalAvgPrices
     //    coins.get("coins").map(c => (c.foreach(cf => println((cf.name, cf.price)))))
     //    println(client.getCoinGlobalAvgPrices("bitcoin", "USD"))
@@ -15,7 +15,7 @@ object Tester {
     //    println(client.getMarketsByCoinId("bitcoin"))
     //    println(client.getTickersByExchangePair("kraken", "BTC-USD"))
     //    println(client.getFiats)
-    val news = client.getNews
+//    val news = client.getNews
 //news.map(_._2).foreach(x => x.map(n => println(n.title, n.link)))
   }
 }
--- src/main/scala/com/trp/coinstats/utils/JSONPickler.scala
+++ src/main/scala/com/trp/coinstats/utils/JSONPickler.scala
@@ -2,14 +2,14 @@ package com.trp.coinstats.utils
 
 object JSONPickler extends upickle.AttributeTagged {
   override implicit def OptionWriter[T: Writer]: Writer[Option[T]] =
-    implicitly[Writer[Option[T]]].comap[Option[T]] {
-      case None => null.asInstanceOf[Option[T]]
-      case Some(x) => Some(x)
+    implicitly[Writer[T]].comap[Option[T]] {
+      case None => null.asInstanceOf[T]
+      case Some(x) => x
     }
 
   override implicit def OptionReader[T: Reader]: Reader[Option[T]] = {
-    new Reader.Delegate[Any, Option[T]](implicitly[Reader[T]].map(Some(_))) {
-      override def visitNull(index: Int) = None
+    new Reader.Delegate[Any, Option[T]](implicitly[Reader[T]].map(Some(_))){
+      override def visitNull(index: Int): Option[T] = None
     }
   }
 }
